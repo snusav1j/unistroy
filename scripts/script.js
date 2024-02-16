d9adaa69817e9bce4006f13dc62e83c708bfab27
samir <106090175+snusav1j@users.noreply.github.com>
2024-02-16 03:49:04 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+//element-animations
+
 const animItems = document.querySelectorAll('.anim-items')
 
 if (animItems.length > 0){
@@ -30,3 +32,68 @@ if (animItems.length > 0){
     }
     animOnScroll();
 }
+
+// slider 
+
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+// let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+        active = key;
+        reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
+const sections = [...document.getElementsByTagName('section')];
+let currentSection = 0;
+
+//scroll
+
+
+// window.addEventListener('wheel', function(e) {
+//     e.preventDefault();
+
+//     (e.deltaY < 0) ? --currentSection: ++currentSection;
+
+//     if (currentSection < 0) currentSection = 0;
+//     else if (currentSection > (sections.length - 1)) currentSection = (sections.length - 1);
+
+//     scrollToSection(currentSection);
+// });
+
+// function scrollToSection(i) {
+//     document.getElementById(sections[i].id).scrollIntoView({
+//     behavior: 'smooth'
+//     });
+// }
